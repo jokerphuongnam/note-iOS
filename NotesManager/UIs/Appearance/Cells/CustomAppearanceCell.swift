d9661a73856d28f68cab0137121f2287f9faa5ac
P. Nam <nam.pp@gannha.com>
2022-08-09 12:36:05 +0700

    Validate text field for time

--- NotesManager/UIs/Appearance/Cells/CustomAppearanceCell.swift
+++ NotesManager/UIs/Appearance/Cells/CustomAppearanceCell.swift
@@ -21,6 +21,8 @@ class CustomAppearanceCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        startTimeTextField.delegate = self
+        endTimeTextField.delegate = self
     }
     
     func setSelected(startTime: Int, endTime: Int) {
@@ -34,48 +36,68 @@ class CustomAppearanceCell: UICollectionViewCell {
         checkImageView.isHidden = true
         timeView.isHidden = true
     }
+    
+    // MARK: - validate minutes
+    fileprivate func validate(_ sender: UITextField, minutes string: String) {
+        if let minutes = Int(string), minutes > 59 {
+            var m = String(minutes % 60)
+            if m.count == 1 {
+                m.insert("0", at: m.startIndex)
+            }
+            
+            sender.text = sender.text?.replacingOccurrences(of: string, with: m)
+        }
+    }
+    
+    // MARK: - validate hours
+    fileprivate func validate(_ sender: UITextField, hours string: String) {
+        if let hours = Int(string), hours > 23 {
+            var h = String(hours % 24)
+            if h.count == 1 {
+                h.insert("0", at: h.startIndex)
+            }
+            
+            sender.text = sender.text?.replacingOccurrences(of: string, with: h)
+        }
+    }
 }
 
 // MARK: - Action
 private extension CustomAppearanceCell {
-    @IBAction private func editingFinished(_ sender: UITextField, forEvent event: UIEvent) {
-        let currentInput = sender.text!
-        if currentInput.count > 5 || currentInput.count < 3 {   // Refuse more than 5 chars as 23:59 or less than 3 chars but accepts 123 as 1:23
-            sender.text = ""    // Clear it
-            // Could play a beep
-            return
+    @IBAction private func textFieldDidChanged(_ sender: UITextField, forEvent event: UIEvent) {
+        guard let text = sender.text else { return }
+        let split = text.split(separator: ":")
+        if split.count > 1, let minutes = split.last {
+            validate(sender, minutes: String(minutes))
         }
-        
-        let posOfColon = currentInput.firstIndex(of: ":")
-        let withColon = posOfColon != nil
-        var hh = ""
-        var mm = ""
-        
-        if withColon {
-            hh = String(currentInput[..<posOfColon!])
-            let mmIndex = currentInput.index(posOfColon!, offsetBy: 1)
-            mm = String(currentInput[mmIndex...])
-        } else {
-            mm = String(currentInput.suffix(2))     // Always 2 digits for minutes
-            if currentInput.count == 4 {  // format as 1234
-                hh = String(currentInput.prefix(2))     // first 2 digits
-            } else {  // Only 3 chars
-                hh = String(currentInput.prefix(1))     // Only one digit here
-            }
+        if let hours = split.first {
+            validate(sender, hours: String(hours))
         }
-        if Int(hh) == nil || Int(hh)! > 23  {
-            sender.text = ""    // Clear it
-            // Could play a beep
-            return
-        }
-        if Int(mm) == nil || Int(mm)! > 59  {
-            sender.text = ""    // Clear it
-            // Could play a beep
-            return
-        }
-        
-        // Use hh and mm as needed
     }
+    
+    @IBAction func textFieldDidEnd(_ sender: UITextField, forEvent event: UIEvent) {
+        guard let text = sender.text else { return }
+        let split = text.split(separator: ":")
+        if split.count > 1 {
+            if let minutes = split.last, minutes.count == 2 {
+                validate(sender, minutes: String(minutes))
+            } else {
+                sender.text?.insert("0", at: text.index(text.startIndex, offsetBy: 3))
+            }
+        } else {
+            sender.text = text + ":00"
+        }
+        
+        if let hours = split.first, hours.count == 2 {
+            validate(sender, hours: String(hours))
+        } else {
+            if text.isEmpty {
+                sender.text?.insert("0", at: text.startIndex)
+            }
+            sender.text?.insert("0", at: text.startIndex)
+        }
+    }
+    
     
     @IBAction private func clockAction(_ sender: UIButton, forEvent event: UIEvent) {
         
@@ -84,5 +106,31 @@ private extension CustomAppearanceCell {
 
 // MARK: - UITextFieldDelegate
 extension CustomAppearanceCell: UITextFieldDelegate {
-    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text, (text + string).count < 6 else {
+            return false
+        }
+        if let index = text.firstIndex(of:":"), text.distance(from: text.startIndex, to: index) == 1 {
+            textField.text = "0" + text
+            return false
+        }
+        
+        if text.count < 2, string == ":" && !text.contains(":") {
+            textField.text = text.isEmpty ? "00" : "0" + text
+        }
+        
+        guard string.isEmpty || string.first?.isWholeNumber ?? false else {
+            return false
+        }
+        
+        if string.isEmpty, text.count == 4 {
+            if let index = text.firstIndex(of: ":") {
+                let substr = text.prefix(through: index)
+                textField.text = String(substr)
+            }
+        } else if !string.isEmpty, text.count == 2 {
+            textField.text = text + ":"
+        }
+        return true
+    }
 }
